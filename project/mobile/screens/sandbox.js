1a9ff09dadd9a2407d862c0d1d88b1b2085a2864
Jack C <chen.jack@email.com>
2018-10-30 20:46:13 -0400

    Draggable item now scrolls with proper positions

--- project/mobile/screens/sandbox.js
+++ project/mobile/screens/sandbox.js
@@ -9,7 +9,17 @@ class Draggable extends React.Component{
 
         this.state = {
             pan: new Animated.ValueXY(),
-            focus : false 
+            focus : false ,
+        }
+
+        this.default_size = {width:0, height:0}
+    }
+
+    _onLayoutHandler = (e)=>{
+        console.log("layout", e.nativeEvent.layout);
+        this.default_size = {
+            width: e.nativeEvent.layout.width,
+            height: e.nativeEvent.layout.height
         }
     }
 
@@ -30,13 +40,26 @@ class Draggable extends React.Component{
                 this.setState({
                     focus: true
                 })
-                console.log("gesture", gestureState);
+                const center_offset = {
+                    x: this.default_size.width/2,
+                    y: this.default_size.height/2
+                }
                 this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value})
-                this.state.pan.setValue({x: gestureState.x0-20 , y: gestureState.y0-20});
+                this.state.pan.setValue({
+                    x: gestureState.x0 - center_offset.x,
+                    y: gestureState.y0 - center_offset.y
+                });
             },
 
             onPanResponderMove : ({nativeEvent}, gestureState) => {
-                this.state.pan.setValue({x: gestureState.moveX, y : gestureState.moveY})
+                const center_offset = {
+                    x: this.default_size.width/2,
+                    y: this.default_size.height/2
+                }
+                this.state.pan.setValue({
+                    x: gestureState.moveX - center_offset.x, 
+                    y: gestureState.moveY - center_offset.y
+                })
             },
         
 
@@ -58,33 +81,35 @@ class Draggable extends React.Component{
             onResponderTerminationRequest: (e,gesturestate) => false,
 
             onPanResponderRelease: (e, gestureState) => {
+                this.setState({
+                    focus: false
+                }, ()=>{
                 
-                this.state.pan.setValue({x: 0, y: 0});
-                this.state.pan.flattenOffset();
-                Animated.spring(
-                    this.state.pan, // The value to drive
-                    {
-                      toValue: {x:0, y:0}, bounciness: 12, speed: 20 // Animate to final value of 1
-                    }
-                  ).start(()=>{
-                    this.setState({
-                        focus: false
-                    })
-                  }); // Start the animation
+                    this.state.pan.setValue({x: 0, y: 0});
+                    this.state.pan.flattenOffset();
+                })
+                // Animated.spring(
+                //     this.state.pan, // The value to drive
+                //     {
+                //       toValue: {x:0, y:0}, bounciness: 12, speed: 20 // Animate to final value of 1
+                //     }
+                //   ).start(()=>{
+                  
+                //   }); // Start the animation
             }
           });
     }
+
     render(){
-      
         // Calculate the transform property and set it as a value for our style which we add below to the Animated.View component
         let imageStyle = {backgroundColor: "purple", transform: this.state.pan.getTranslateTransform()};
         return ( 
-            <Animated.View style={[imageStyle]} {...this._panResponder.panHandlers}>
+            <View {...this._panResponder.panHandlers}>
                 {this.state.focus ? 
                 <Modal
                     visible = {true}
                     transparent = {true}>
-                    <Animated.View style={imageStyle} >
+                    <Animated.View style={[imageStyle, {width: this.default_size.width, height: this.default_size.height}]} >
                     {/* <Animated.View style={[imageStyle]} {...this._panResponder.panHandlers}> */}
                         <Text> Move please</Text>
                         <Text> TEST </Text>
@@ -92,13 +117,13 @@ class Draggable extends React.Component{
                     </Animated.View>
                 </Modal>
                 :
-                <View>
+                <View style={{backgroundColor: "yellow"}} onLayout={this._onLayoutHandler}>
                 {/* <Animated.View style={[imageStyle]} {...this._panResponder.panHandlers}> */}
                     <Text> Move please</Text>
                     {this.props.children}
                 {/* </Animated.View> ) */}
                 </View>}
-            </Animated.View>)
+            </View>)
     }
 }
 
